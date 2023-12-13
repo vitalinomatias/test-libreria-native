04e659ed499a662bc1ce4a3ea1575eb91e4e7b37
vitalinomatias <vitalino.1990@gmail.com>
2023-12-13 14:45:19 -0600

    cambio de color al boton

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -2,7 +2,7 @@ import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'red',
+    backgroundColor: 'green',
     padding: 10,
     borderRadius: 10,
     alignItems: 'center',
